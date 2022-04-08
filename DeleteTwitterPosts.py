5abadfba59d27eb88e1ec04e2206c0f0af1268ca
PyMan142 <91704113+PyMan142@users.noreply.github.com>
2022-04-08 01:36:18 -0400

    Delete your tweets / posts using Tweepy
    Hope this saves at least someone a huge headache, enjoy! Leave a like for a homie!

---
+++ DeleteTwitterPosts.py
@@ -0,0 +1,32 @@
+import time
+import json
+import tweepy
+
+# assign the values accordingly
+api_key = ""
+api_secret = ""
+access_token = ""
+access_token_secret = ""
+
+# Start Loop
+
+while True:
+    # authorization of api key and api secret
+    auth = tweepy.OAuthHandler(api_key, api_secret)
+
+    # set access to user's access key and access secret 
+    auth.set_access_token(access_token, access_token_secret)
+
+    # calling the api 
+    api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
+
+    #Get json data
+    json_data = api.user_timeline(screen_name="@Username")
+    data = json.dumps(json_data)
+    parsed = json.loads(data)
+
+    for item in parsed:
+        postID = item.get("id")
+        print(postID)
+        api.destroy_status(id=postID)
+        time.sleep(0.5)
